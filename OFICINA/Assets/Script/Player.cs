7ed3026b7a72558b1788a126f095d2951f45dd70
Mayra Regina Nascimento de Oliveira <mayra.regina@academico.ifrn.edu.br>
2023-10-26 16:00:14 -0300

    10/26

--- OFICINA/Assets/Script/Player.cs
+++ OFICINA/Assets/Script/Player.cs
@@ -9,14 +9,15 @@ public class Player : MonoBehaviour
     public float jumpForce;
 
     public GameObject Bala;
+    public GameObject Bala2;
     public Transform firePoint;
     
-    private bool isJumping;
-    private bool doubleJump;
-    private bool isFire;
-    private bool stage1 = true;
-    private bool stage2;
-    private bool stage3;
+    public bool isJumping;
+    public bool doubleJump;
+    public bool isFire;
+    public bool stage1 = true;
+    public bool stage2;
+    public bool stage3;
     
 
     private Rigidbody2D rig;
@@ -24,15 +25,12 @@ public class Player : MonoBehaviour
 
     private float movement;
     
-    // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         Jump();
@@ -54,7 +52,7 @@ public class Player : MonoBehaviour
         {
             if (!isJumping)
             {
-                anim.SetInteger("transicao",1);
+                anim.SetInteger("Transition",3);
             }
             
             transform.eulerAngles = new Vector3(0, 0, 0);
@@ -64,7 +62,7 @@ public class Player : MonoBehaviour
         {
             if (!isJumping)
             {
-                anim.SetInteger("transicao",1);   
+                anim.SetInteger("Transition",3);   
             }
             
             transform.eulerAngles = new Vector3(0, 180, 0);
@@ -72,7 +70,7 @@ public class Player : MonoBehaviour
 
         if(movement == 0 && !isJumping && !isFire)
         {
-            anim.SetInteger("transicao", 0);
+            anim.SetInteger("Transition", 0);
         }
     }
 
@@ -82,16 +80,16 @@ public class Player : MonoBehaviour
         {
             if(!isJumping)
             {
-                anim.SetInteger("transicao",2);
+                anim.SetInteger("Transition",1);
                 rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
                 isJumping = true;
             }
             else
             {
-                if(doubleJump)
+                if(doubleJump && stage2 == true)
                 {
-                    anim.SetInteger("transicao",2);
+                    anim.SetInteger("Transition",1);
                     rig.AddForce(new Vector2(0, jumpForce * 1), ForceMode2D.Impulse);
                     doubleJump = false;
                 }
@@ -109,21 +107,19 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             isFire = true;
-            anim.SetInteger("transicao", 3);
-            GameObject bala = Instantiate(Bala, firePoint.position, firePoint.rotation);
-
-            if(transform.rotation.y == 0)
+            anim.SetInteger("Transition", 4);
+            if (stage1 == true)
             {
-                
+                GameObject bala = Instantiate(Bala, firePoint.position, firePoint.rotation);
             }
 
-            if(transform.rotation.y == 180)
+            if (stage3 == true)
             {
+                GameObject bala2 = Instantiate(Bala2, firePoint.position, firePoint.rotation);
             }
-            
             yield return new WaitForSeconds(0.3f);
             isFire = false;
-            anim.SetInteger("transicao",0);
+            anim.SetInteger("Transition",0);
         }
     }
 
