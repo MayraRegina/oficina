6b807521225512b26bd64f6cc70747861d56ca6e
Mayra Regina Nascimento de Oliveira <mayra.regina@academico.ifrn.edu.br>
2023-10-26 14:45:27 -0300

    26/10

--- OFICINA/Assets/Script/Player.cs
+++ OFICINA/Assets/Script/Player.cs
@@ -4,32 +4,141 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Atributos")] 
-    public float jump;
+    public int health = 7;
     public float speed;
-    public int life;
-    public bool isjump;
-    public bool doublejump;
+    public float jumpForce;
 
-    [Header("Componentes")] 
-    public Rigidbody rig;
-    public Animator anim;
+    public GameObject Bala;
+    public Transform firePoint;
     
+    private bool isJumping;
+    private bool doubleJump;
+    private bool isFire;
+    private bool stage1 = true;
+    private bool stage2;
+    private bool stage3;
+    
+
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    private float movement;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        rig = GetComponent<Rigidbody>();
+        rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+        balaFire();
     }
 
-    void move()
+    void FixedUpdate()
     {
-        float moviment = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector3(moviment * speed, rig.velocity.y);
+        Move();
+    }
+
+    void Move()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+
+        if (movement > 0)
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transicao",1);
+            }
+            
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } 
+
+        if (movement < 0 )
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transicao",1);   
+            }
+            
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if(movement == 0 && !isJumping && !isFire)
+        {
+            anim.SetInteger("transicao", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+            {
+                anim.SetInteger("transicao",2);
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                isJumping = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    anim.SetInteger("transicao",2);
+                    rig.AddForce(new Vector2(0, jumpForce * 1), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+    }
+
+    void balaFire()
+    {
+       StartCoroutine("Fire");
+    }
+
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isFire = true;
+            anim.SetInteger("transicao", 3);
+            GameObject bala = Instantiate(Bala, firePoint.position, firePoint.rotation);
+
+            if(transform.rotation.y == 0)
+            {
+                
+            }
+
+            if(transform.rotation.y == 180)
+            {
+            }
+            
+            yield return new WaitForSeconds(0.3f);
+            isFire = false;
+            anim.SetInteger("transicao",0);
+        }
+    }
+
+   
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == 6)
+        {
+            isJumping = false;
+        }
+
+        if (coll.gameObject.tag == "espada")
+        {
+            health--;
+        }
     }
 }
